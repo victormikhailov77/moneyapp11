0c11dcf5e678a1fa36cd1645ce42775b2c54ac31
victor.mikhaylov <V@rde2017>
2019-09-24 11:56:07 +0200

    Keys refreshed

--- transfer-service/src/main/java/org/fastpay/service/AccountRestClient.java
+++ transfer-service/src/main/java/org/fastpay/service/AccountRestClient.java
@@ -16,76 +16,41 @@ import java.util.Map;
 @Component
 public class AccountRestClient implements AccountService
 {
-
-    private final String serviceUrl = "http://localhost:8095/api/accounts/";
+    private final String accountRestApiUrl = "http://localhost:8095/api/accounts/";
+    private final String accountRestApiDepositUrl = accountRestApiUrl + "deposit";
 
     @Autowired
     private OAuth2RestTemplate oAuth2RestTemplate;
 
     @Override
-    public PaymentStatus authorizePayment(String source, String destination, BigDecimal amount, String currency, String txID) {
-
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        Map map = new HashMap<String, String>();
-        map.put("Content-Type", "application/json");
-        headers.setAll(map);
+    public PaymentStatus authorizePayment(String source, String destination, BigDecimal amount, String currency, String txId) {
         Map<String, String> payload = new HashMap<>();
         payload.put("source", source);
         payload.put("destination", destination);
         payload.put("amount", amount.toString());
         payload.put("currency", currency);
-        payload.put("txid", txID);
-        HttpEntity<?> request = new HttpEntity<>(payload, headers);
-        ResponseEntity<PaymentStatus> result = oAuth2RestTemplate.postForEntity(serviceUrl, request, PaymentStatus.class);
-        return result.getBody();
+        payload.put("txid", txId);
+        return restPostHelper(accountRestApiUrl, payload);
     }
 
     @Override
     public PaymentStatus cancelPayment(String txId) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        Map map = new HashMap<String, String>();
-        map.put("Content-Type", "application/json");
-        headers.setAll(map);
-        HttpEntity<?> request = new HttpEntity<>(null, headers);
-        ResponseEntity<PaymentStatus> result = oAuth2RestTemplate.exchange(serviceUrl + "{id}",
-                HttpMethod.DELETE,
-                request,
-                PaymentStatus.class,
-                txId
-        );
-        return result.getBody();
+        return restExchangeHelper(txId, HttpMethod.DELETE);
     }
 
     @Override
     public PaymentStatus deposit(String destination, BigDecimal amount, String currency, String txId) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        Map map = new HashMap<String, String>();
-        map.put("Content-Type", "application/json");
-        headers.setAll(map);
         Map<String, String> payload = new HashMap<>();
         payload.put("destination", destination);
         payload.put("amount", amount.toString());
         payload.put("currency", currency);
         payload.put("txid", txId);
-        HttpEntity<?> request = new HttpEntity<>(payload, headers);
-        ResponseEntity<PaymentStatus> result = oAuth2RestTemplate.postForEntity(serviceUrl + "deposit", request, PaymentStatus.class);
-        return result.getBody();
+        return restPostHelper(accountRestApiDepositUrl, payload);
     }
 
     @Override
     public PaymentStatus finalizePayment(String txId) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        Map map = new HashMap<String, String>();
-        map.put("Content-Type", "application/json");
-        headers.setAll(map);
-        HttpEntity<?> request = new HttpEntity<>(null, headers);
-        ResponseEntity<PaymentStatus> result = oAuth2RestTemplate.exchange(serviceUrl + "{id}",
-                HttpMethod.PUT,
-                request,
-                PaymentStatus.class,
-                txId
-        );
-        return result.getBody();
+        return restExchangeHelper(txId, HttpMethod.PUT);
     }
 
     @Override
@@ -96,5 +61,30 @@ public class AccountRestClient implements AccountService
     @Override
     public void createAccount(String origin_account, String currency) {
 
+    }
+
+    private PaymentStatus restExchangeHelper(String txId, HttpMethod httpVerb) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        Map map = new HashMap<String, String>();
+        map.put("Content-Type", "application/json");
+        headers.setAll(map);
+        HttpEntity<?> request = new HttpEntity<>(null, headers);
+        ResponseEntity<PaymentStatus> result = oAuth2RestTemplate.exchange(accountRestApiUrl + "{id}",
+            httpVerb,
+            request,
+            PaymentStatus.class,
+            txId
+        );
+        return result.getBody();
+    }
+
+    private PaymentStatus restPostHelper(String url, Map<String, String> payload) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        Map map = new HashMap<String, String>();
+        map.put("Content-Type", "application/json");
+        headers.setAll(map);
+        HttpEntity<?> request = new HttpEntity<>(payload, headers);
+        ResponseEntity<PaymentStatus> result = oAuth2RestTemplate.postForEntity(url, request, PaymentStatus.class);
+        return result.getBody();
     }
 }
