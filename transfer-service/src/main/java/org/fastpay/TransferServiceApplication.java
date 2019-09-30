0473a00f35a83c2dadc136e851361bd1e02ac47c
victor.mikhaylov <V@rde2017>
2019-09-30 17:13:41 +0200

    Spring configuration moved to external Maven module

--- transfer-service/src/main/java/org/fastpay/TransferServiceApplication.java
+++ transfer-service/src/main/java/org/fastpay/TransferServiceApplication.java
@@ -1,13 +1,16 @@
 package org.fastpay;
 
+import org.fastpay.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {
 	"org.fastpay"}, exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@Import( {SecurityConfigurer.class, SecurityProperties.class, SecurityContextUtils.class, OAuth2RestTemplateConfigurer.class, JwtAccessTokenCustomizer.class})
 public class TransferServiceApplication {
 
 	public static void main(String[] args) {
